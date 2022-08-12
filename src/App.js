c9716777a76fd6d1038b20610cda493b6a3b48cd
akshit <akshit.kanani.contractor@eqonex.com>
2022-08-12 14:28:39 +0530

    without schema validate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Login from './login';
 import Register from './register';
 
 function App() {
   return (
     <div className="App">
         <Register/>
+        {/* <Login/> */}
     </div>
   );
 }
--- src/register.js
+++ src/register.js
@@ -1,35 +1,78 @@
 import { useFormik } from "formik";
 import React from "react";
-import * as Yup from "yup";
+// import * as Yup from "yup";
+
+const initialValues= {
+  username: "",
+  name: "",
+  email: "",
+  status: "",
+  country:"",
+}
+
+const validate = values =>{
+  // console.log("values",values);
+    const errors={}
+    if(!values.username)
+    {
+      errors.username="Requied"
+    }
+    
+    if(!values.name)
+    {
+      errors.name="Requied"
+    }
+     
+    if(!values.email)
+    {
+      errors.email="Requied"
+    }
+    else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))
+    {
+      errors.email="Invalid email format"
+    }
+
+    if(!values.status)
+    {
+      errors.status="Requied"
+    }
+
+    if(!values.country)
+    {
+      errors.country="Requied"
+    }
+    return errors
+}
+
+// const validationSchema=Yup.object({
+//   username:Yup.string()
+//               .max(10,"username must be less 10 character")
+//               .required("this  is required field"),
+//   name:Yup.string()
+//           .required("this is required field"),
+//   email:Yup.string()
+//             .email("please provide valid Email")
+//             .required("this is required field"),
+//   status:Yup.string()
+//             .required("this is required field"),
+//   country:Yup.string()
+//             .required("this is required field"),  
+// })
+
+const onSubmit= (values) => {
+  console.log("form submit", values);
+  // localStorage.setItem("formikkishan",JSON.stringify([values]))
+}
+
 const Register = () => {
   const formik = useFormik({
-    initialValues: {
-      username: "",
-      name: "",
-      email: "",
-      status: "",
-      country:"",
-    },
-    validationSchema:Yup.object({
-        username:Yup.string()
-                    .max(10,"username must be less 10 character")
-                    .required("this  is required field"),
-        name:Yup.string()
-                .required("this is required field"),
-        email:Yup.string()
-                  .email("please provide valid Email")
-                  .required("this is required field"),
-        status:Yup.string()
-                  .required("this is required field"),
-        country:Yup.string()
-                  .required("this is required field"),  
-    }),
-    onSubmit: (values) => {
-      console.log("form submit", values);
-    },
+    initialValues,
+    validate,
+    // validationSchema,
+    onSubmit
   });
 
-    // console.log("formikError", formik.touched);
+    console.log("formikError", formik.errors);
 
   return (
     <div>
@@ -102,6 +145,10 @@ const Register = () => {
 
         <br />
         <br />  
+
+        
+
+
         <button type="submit">Submit</button>
       </form>
     </div>
